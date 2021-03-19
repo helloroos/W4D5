e660846307ede9e7a8de78f657512391ed7d8dde
Daniel <danielwu116@gmail.com>
2021-03-19 16:39:22 -0400

    Add #bad_two_sum?

--- two_sum.rb
+++ two_sum.rb
@@ -1,11 +1,20 @@
 def bad_two_sum?(arr, target_sum)
-    arr.each_with_index do |ele1, i|
-        arr.each_with_index do |ele2, j|
-            if j > i 
-                return true if ele1 + ele2 == target_sum
+    arr.each_with_index do |ele1, i|                        # O(n)
+        arr.each_with_index do |ele2, j|                    # O(n)
+            if j > i                                        # O(1)
+                return true if ele1 + ele2 == target_sum    # O(1)
             end
         end
     end
-    false 
-end
+    false                                                   # O(1)
+end 
+
+# n * (n * (1 + 1)) + 1
+# n * n
+# n^2
+
+arr = [0, 1, 5, 7]
+
+p bad_two_sum?(arr, 6)
+p bad_two_sum?(arr, 10)
 
